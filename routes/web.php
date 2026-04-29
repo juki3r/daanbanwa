f26cd44007f6d64663fdd1f299ee128949b83ead
juki3r <deepong25@gmail.com>
2026-04-29 17:10:35 +0800

    aaa

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 
 
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

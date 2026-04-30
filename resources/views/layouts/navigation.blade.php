5daa48f8c6b84cfd0aec9de94fb5adca295208be
juki3r <deepong25@gmail.com>
2026-04-30 16:38:11 +0800

    aaa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,6 +31,12 @@
                         </x-nav-link>
                     </div>
 
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('admin.ordinances')" :active="request()->routeIs('admin.ordinances')">
+                            {{ __('Ordinances Management') }}
+                        </x-nav-link>
+                    </div>
+
                 @endif
             </div>
 

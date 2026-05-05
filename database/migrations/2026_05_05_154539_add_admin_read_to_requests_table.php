2c8664a422d65847e59b2d57ff1d777519e28e62
juki3r <deepong25@gmail.com>
2026-05-05 15:56:22 +0800

    aa

--- database/migrations/2026_05_05_154539_add_admin_read_to_requests_table.php
+++ database/migrations/2026_05_05_154539_add_admin_read_to_requests_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('requests', function (Blueprint $table) {
-            //
+            $table->boolean('admin_read')->default(false)->after('status');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('requests', function (Blueprint $table) {
-            //
+            $table->dropColumn('admin_read');
         });
     }
 };

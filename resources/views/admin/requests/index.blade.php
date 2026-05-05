cf1de8521bca4fa562bfbda75ced3e32396cb16e
juki3r <deepong25@gmail.com>
2026-05-05 14:23:06 +0800

    aa

--- resources/views/admin/requests/index.blade.php
+++ resources/views/admin/requests/index.blade.php
@@ -175,7 +175,7 @@
                             <option value="">Choose resident</option>
                             @foreach($users as $user)
                                 <option value="{{ $user->id }}">
-                                    {{ $user->name }} - {{ $user->email }}
+                                    {{ $user->first_name }} {{ $user->middle_name ?? '' }} {{ $user->last_name }}
                                 </option>
                             @endforeach
                         </select>

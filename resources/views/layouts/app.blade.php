ea998d90091b90b68d38245b328a92348e3029d2
juki3r <deepong25@gmail.com>
2026-05-05 14:20:25 +0800

    aa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,10 @@
         <link rel="shortcut icon" href="{{asset('/images/logo.png')}}" type="image/x-icon">
 
         <link rel="stylesheet" href="{{asset('/bootstrap/bootstrap.css')}}">
-        <script src="{{asset('/bootstrap/bootstrap.js')}}"></script>
+        {{-- <script src="{{asset('/bootstrap/bootstrap.js')}}"></script> --}}
 
-        {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

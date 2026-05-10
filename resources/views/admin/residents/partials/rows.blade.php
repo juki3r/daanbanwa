fd588d24e927ae6e0b74d4796f8c9448118c28aa
juki3r <deepong25@gmail.com>
2026-05-10 20:25:38 +0800

    aa

--- resources/views/admin/residents/partials/rows.blade.php
+++ resources/views/admin/residents/partials/rows.blade.php
@@ -1,44 +1,47 @@
 @forelse($residents as $resident)
 
 <tr>
+    {{-- NAME --}}
     <td>
-    {{ trim(
-        $resident->first_name . ' ' .
-        ($resident->middle_name ? $resident->middle_name . ' ' : '') .
-        $resident->last_name .
-        ($resident->suffix ? ' ' . $resident->suffix : '')
-    ) }}
-    </td>   
+        {{ trim(
+            $resident->first_name . ' ' .
+            ($resident->middle_name ? $resident->middle_name . ' ' : '') .
+            $resident->last_name .
+            ($resident->suffix ? ' ' . $resident->suffix : '')
+        ) }}
+    </td>
+
     <td>{{ $resident->purok }}</td>
 
     <td>{{ $resident->household_name }}</td>
 
     <td>{{ $resident->sex }}</td>
 
-    <td>{{ $resident->birth_date }}</td>
+    {{-- DATE FORMAT FIX --}}
+    <td>{{ \Carbon\Carbon::parse($resident->birth_date)->format('M d, Y') }}</td>
 
+    {{-- AGE --}}
     <td>{{ $resident->age ?? \Carbon\Carbon::parse($resident->birth_date)->age }}</td>
 
     <td>{{ $resident->civil_status }}</td>
 
+    {{-- VOTER --}}
     <td>
-        @if($resident->is_voter)
-            <span class="badge bg-success">Yes</span>
-        @else
-            <span class="badge bg-secondary">No</span>
-        @endif
+        <span class="badge {{ $resident->is_voter ? 'bg-success' : 'bg-secondary' }}">
+            {{ $resident->is_voter ? 'Yes' : 'No' }}
+        </span>
     </td>
 
+    {{-- PWD --}}
     <td>
-        @if($resident->is_pwd)
-            <span class="badge bg-warning text-dark">Yes</span>
-        @else
-            <span class="badge bg-secondary">No</span>
-        @endif
+        <span class="badge {{ $resident->is_pwd ? 'bg-warning text-dark' : 'bg-secondary' }}">
+            {{ $resident->is_pwd ? 'Yes' : 'No' }}
+        </span>
     </td>
 
+    {{-- STATUS --}}
     <td>
-        <span class="badge 
+        <span class="badge
             @if($resident->resident_status == 'Active') bg-success
             @elseif($resident->resident_status == 'Moved Out') bg-warning
             @else bg-danger @endif">
@@ -50,13 +53,9 @@
 @empty
 
 <tr>
-    <td colspan="12" class="text-center text-muted py-3">
+    <td colspan="10" class="text-center text-muted py-3">
         No records found
     </td>
 </tr>
 
 @endforelse
-
-
-
-

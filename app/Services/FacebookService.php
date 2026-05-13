31b26f7afe072877c527a40766c0a41c817a66f5
juki3r <deepong25@gmail.com>
2026-05-13 11:55:32 +0800

    aaa

--- app/Services/FacebookService.php
+++ app/Services/FacebookService.php
@@ -1,21 +1,3 @@
-// use Illuminate\Support\Facades\Http;
-
-// class FacebookService
-// {
-// public function postToPage($message)
-// {
-// $pageId = '863707336834066';
-// $token = 'EAA4c3g4kZCP8BRSqKLhJ1aRf5ApKCJYZBjqhOodJ5WhtLYlmm4fnabZCciXnMZCb4I3PLfFqoTrZCtKhqRQhZC70WcnPfqZBU0ZB2B4Ue4FLnE3gKvFBztI4prRhiDXhfDAXZAxsDrBsPmArSBUJXVS49nHpap7sf672FUdrTAG2QgzRaXsa33bBC7vdF369Kl6UOpMn2EgASZBwiaKK4ry7djZBYbJSSiLIRFIbg5ksRSvHlEZD';
-
-// $url = "https://graph.facebook.com/{$pageId}/feed";
-
-// return Http::post($url, [
-// 'message' => $message,
-// 'access_token' => $token,
-// ])->json();
-// }
-// }
-
 <?php
 
 namespace App\Services;
@@ -28,7 +10,6 @@ class FacebookService
     {
         $pageId = '863707336834066';
         $token = 'EAA4c3g4kZCP8BRSqKLhJ1aRf5ApKCJYZBjqhOodJ5WhtLYlmm4fnabZCciXnMZCb4I3PLfFqoTrZCtKhqRQhZC70WcnPfqZBU0ZB2B4Ue4FLnE3gKvFBztI4prRhiDXhfDAXZAxsDrBsPmArSBUJXVS49nHpap7sf672FUdrTAG2QgzRaXsa33bBC7vdF369Kl6UOpMn2EgASZBwiaKK4ry7djZBYbJSSiLIRFIbg5ksRSvHlEZD';
-
 
         $url = "https://graph.facebook.com/{$pageId}/feed";
 

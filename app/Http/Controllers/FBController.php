a28ba484fbd16ce2f4b1123bd489250d819374c4
juki3r <deepong25@gmail.com>
2026-05-13 11:49:23 +0800

    aa

--- app/Http/Controllers/FBController.php
+++ app/Http/Controllers/FBController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 
-use FacebookService;
 use Illuminate\Http\Request;
+use App\Services\FacebookService;
 
 
 class FBController extends Controller
 {
-    function postToFacebook(Request $request)
+    // function postToFacebook(Request $request)
+    // {
+    //     // $message = $request->input('message');
+    //     $message = "Hello, this is a test post from server to Facebook!";
+
+    //     $fb = new FacebookService();
+    //     $response = $fb->postToPage($message);
+
+    //     return response()->json($response);
+    // }
+
+    public function postToFacebook(Request $request, FacebookService $fb)
     {
-        // $message = $request->input('message');
         $message = "Hello, this is a test post from server to Facebook!";
 
-        $fb = new FacebookService();
         $response = $fb->postToPage($message);
 
         return response()->json($response);

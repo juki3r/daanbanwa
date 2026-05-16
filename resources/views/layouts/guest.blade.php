069935fd9f282dcaa4ec269bcb445ec592a733dd
juki3r <deepong25@gmail.com>
2026-05-16 18:14:15 +0800

    aa

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,12 +13,18 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        .logo {
+            width: 80px;
+            margin-bottom: 15px;
+        }
+    </style>
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                   <img src="{{asset('images/logo.png')}}" class="logo" alt="Logo">
                 </a>
             </div>
 

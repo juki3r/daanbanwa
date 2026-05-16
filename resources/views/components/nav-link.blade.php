650fe270eeff025b9b8e61ea1dafd9e5de0ccb8e
juki3r <deepong25@gmail.com>
2026-05-16 16:14:42 +0800

    aaa

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-0.5 py-0 border-b-2 border-indigo-400 text-[11px] font-medium leading-none text-gray-900'
-            : 'inline-flex items-center px-0.5 py-0 border-b-2 border-transparent text-[11px] font-medium leading-none text-gray-500 hover:text-gray-700 hover:border-gray-300';
+            : 'inline-flex items-center px-0.5 py-0 border-b-2 border-transparent text-[11px] font-medium leading-none text-gray-500 hover:text-gray-700 hover:border-gray-500';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>

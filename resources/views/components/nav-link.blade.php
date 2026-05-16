c295da618e845c1be3ce9bd27492c1f8791b4da4
juki3r <deepong25@gmail.com>
2026-05-16 16:15:54 +0800

    aa

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-0.5 py-0 border-b-2 border-indigo-400 text-[11px] font-medium leading-none text-gray-900'
+            ? 'inline-flex items-center px-0.5 py-0 border-b-1 border-indigo-400 text-[11px] font-medium leading-none text-gray-900'
             : 'inline-flex items-center px-0.5 py-0 border-b-2 border-transparent text-[11px] font-medium leading-none text-gray-500 hover:text-gray-700 hover:border-gray-500';
 @endphp
 

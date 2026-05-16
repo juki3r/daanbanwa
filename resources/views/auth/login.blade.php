07458fed111ab8b03b10288a7a6127b01590a496
juki3r <deepong25@gmail.com>
2026-05-16 18:33:26 +0800

    aa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,13 +36,13 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="block mt-4">
+        {{-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" name="remember"
                     class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> --}}
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button class="ms-3">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -51,7 +51,7 @@
 
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4  overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 border overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>

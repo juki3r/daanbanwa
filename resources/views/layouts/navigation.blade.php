a5443be89dd0c0728949873a0f291a703b2ec7ce
juki3r <deepong25@gmail.com>
2026-05-16 16:55:51 +0800

    aaa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -108,7 +108,7 @@
     </nav>
 
     {{-- Logout --}}
-    <div class="mt-auto pt-3 border-top" style="color: border-top-color: gray !important">
+    <div class="mt-auto pt-3 border-top" style="border-bottom-color: gray !important">
         <form method="POST" action="{{ route('logout') }}">
             @csrf
             <a href="{{ route('logout') }}"
